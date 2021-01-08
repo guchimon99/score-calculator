7a9da3c579603d5c5d03b279f4d95ee234f50e78
guchimon99 <guchimon99@gmail.com>
2021-01-08 17:28:48 +0900

    [UPDATE] App.js の構成

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,13 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-
 import { Provider as EntitiesProvider } from '../hooks/entities'
 import { Provider as AuthProvider } from '../hooks/auth'
 
-import Library from './Library'
-import Search from './Search'
-import Settings from './Settings'
-import Editor from './Editor'
-
-import Calculators from './Calculators'
-
 import Splash from './Splash'
-import NotFound from './NotFound'
+import Main from './Main'
 
 const App = () => (
   <AuthProvider>
     <EntitiesProvider>
-      <Router>
-        <Switch>
-          <Redirect path="/" exact to="/library" />
-          <Route path="/library" component={Library} />
-          <Route path="/search" component={Search} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/calculators" component={Calculators} />
-          <Route path="/editor" component={Editor} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
+      <Main />
       <Splash />
     </EntitiesProvider>
   </AuthProvider>
---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+
+import { useIsReady } from '../hooks/auth'
+
+import Library from './Library'
+import Search from './Search'
+import Settings from './Settings'
+import Editor from './Editor'
+import NotFound from './NotFound'
+import Calculators from './Calculators'
+
+const Main = () => {
+  const isReady = useIsReady()
+
+  if (!isReady) return null
+  return (
+    <Router>
+      <Switch>
+        <Redirect path="/" exact to="/library" />
+        <Route path="/library" component={Library} />
+        <Route path="/search" component={Search} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/calculators" component={Calculators} />
+        <Route path="/editor" component={Editor} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default Main
