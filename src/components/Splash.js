54455bc96abf18a849abf14199ec22f018a3bf7b
guchimon99 <guchimon99@gmail.com>
2021-01-08 17:28:16 +0900

    [FIX] Splash の表示ロジック

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,11 +5,11 @@ import { useIsReady } from '../hooks/auth'
 const Root = styled.div.attrs({
   className: 'fixed w-full h-full bg-white flex flex-col items-center justify-center top-0'
 })`
-  top: ${({ isReady }) => isReady ? 0 : -100}%;
-  opacity: ${({ isReady }) => isReady ? 0 : 1};
-  transition: ${({ isReady }) => {
-    if (isReady) {
-      return 'top 0s ease 0s, opacity .5s ease .5s'
+  top: ${({ isShown }) => isShown ? 0 : -100}%;
+  opacity: ${({ isShown }) => isShown ? 1 : 0};
+  transition: ${({ isShown }) => {
+    if (isShown) {
+      return 'top 0s ease .5s, opacity .5s ease .5s'
     }
     return 'top 0s ease .5s, opacity .5s ease 0s'
   }};
