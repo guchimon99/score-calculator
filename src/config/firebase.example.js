c2bb7b18ded1b18e56bb578eb3b0200bfae6d54f
guchimon99 <guchimon99@gmail.com>
2021-01-08 15:47:46 +0900

    [ADD] set up firebase

---
+++ src/config/firebase.example.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: '****',
+  authDomain: '****',
+  projectId: '****',
+  storageBucket: '****',
+  messagingSenderId: '****',
+  appId: '****',
+  measurementId: '****'
+}
