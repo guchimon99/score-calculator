f0b8e3d6d7e569c61fa93d4ca853de4cc2ac5cb1
guchimon99 <guchimon99@gmail.com>
2021-01-08 15:03:52 +0900

    [FIX] className が指定されていない時に undefined が追加されるバグ

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -1,5 +1,5 @@
 const Container = ({ className, ...props }) => (
-  <div className={`${className} max-w-2xl mx-auto`} {...props} />
+  <div className={`${className || ''} max-w-2xl mx-auto`} {...props} />
 )
 
 export default Container
