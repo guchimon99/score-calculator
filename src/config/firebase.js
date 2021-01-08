20f894ee8fe6dadf4f732f209f3f7719d97f4dde
guchimon99 <guchimon99@gmail.com>
2021-01-08 18:12:31 +0900

    [ADD] config ファイルの追加

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: 'AIzaSyCQYPgHbJv4MCwc35Yh9WX37462l2iDedA',
+  authDomain: 'score-calculator-55902.firebaseapp.com',
+  projectId: 'score-calculator-55902',
+  storageBucket: 'score-calculator-55902.appspot.com',
+  messagingSenderId: '214053310720',
+  appId: '1:214053310720:web:7aec04e0a45d9df168e3e5',
+  measurementId: 'G-KVB7CKV9VL'
+}
